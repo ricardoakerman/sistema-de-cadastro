267fa4c4a41e86b784d9e7e18e09c4a22badf0c2
ricardoakerman <rakerman@DESKTOP-QO01K0O>
2023-08-31 18:52:20 -0300

    Alteração do Paulo no app js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-
+console.log("O Paulo fez esse commit sem vc ver na aula")
