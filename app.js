47adc4f8205e28ef6a085a0c6b02a53ac4bd0f5c
ricardoakerman <rakerman@DESKTOP-QO01K0O>
2023-09-02 09:53:38 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("O Paulo fez esse commit sem vc ver na aula")
+console.log("Aplicando correção teste")
